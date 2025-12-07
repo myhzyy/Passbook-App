f9b94f7e0b7061edaf07be09ef8c300be70f8316
myhzy <notmyhzy@hotmail.com>
2025-12-07 16:42:11 +0000

    added profile tab

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pawbook.Data;
 using System.Text.Json;
+using Pawbook.Models;
 
 namespace Pawbook.Controllers;
 
@@ -27,5 +28,34 @@ public class UserController : Controller
             .FirstOrDefaultAsync(u => u.Id == id);
         
         return View(profile);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> PostedImage(int id, int postId)
+    {
+
+        var user  = await _context.Users.Include(u => u.Posts).FirstOrDefaultAsync(u => u.Id == id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+        
+        var post = user.Posts.FirstOrDefault(p => p.Id == postId);
+
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        var dto = new PostedImageDto
+        {
+            Caption = post.Caption,
+            ImageUrl = post.ImageUrl,
+            UserName = user.Username
+        };
+        
+        
+        return View(dto);
     }
 }
---
+++ Models/PostedImageDto.cs
@@ -0,0 +1,9 @@
+namespace Pawbook.Models;
+
+public class PostedImageDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string ImageUrl {get; set;} = string.Empty;
+    public string Caption { get; set; } = string.Empty;
+    
+}
